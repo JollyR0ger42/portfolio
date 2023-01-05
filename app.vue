94c2591430b8fc529ee6326dbaa841acf3d8d431
JollyR0ger <kav993@gmail.com>
2023-01-05 17:55:42 +0200

    starting styles

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app__wrapper">
     <div class="app__container">
-      Yo
+      Yo00000000000000000000000000000000000000ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss000000
     </div>
   </div>
 </template>
@@ -9,7 +9,7 @@
 <style lang="scss">
 .app {
   &__wrapper {
-    background-color: var(--dark-blue);
+    background-color: #044463;
     width: 100vw;
     height: 100vh;
     display: flex;
@@ -18,9 +18,11 @@
   }
 
   &__container {
+    color: #fff;
     max-width: 800px;
-    background-color: var(--light-blue);
+    background-color: var(--dark-blue);
     flex: 1;
+    box-shadow: 0px 0px 10px #000;
   }
 }
 </style>
