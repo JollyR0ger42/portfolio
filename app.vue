afa18b9ba5884b60f9edede08f655730eff53ce4
JollyR0ger <kav993@gmail.com>
2023-01-05 04:00:28 +0200

    init commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app__wrapper">
+    <div class="app__container">
+      Yo
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  &__wrapper {
+    background-color: var(--dark-blue);
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__container {
+    max-width: 800px;
+    background-color: var(--light-blue);
+    flex: 1;
+  }
+}
+</style>
