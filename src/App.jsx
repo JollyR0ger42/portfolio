19311374da6c2fe88f8b3a7977e0ec01b304b665
JollyR0ger <kav993@gmail.com>
2023-04-17 22:18:54 +0300

    add scrollVelocity

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Experience, About, Navbar, Works, Footer } from "./components";
 
+import { useMotionValue } from 'framer-motion';
+
 const App = () => {
+  const scrollY = useMotionValue(0);
+
+  window.addEventListener('scroll', () => {
+    scrollY.set(window.scrollY);
+    console.log(scrollY.getVelocity());
+  });
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-app'>
