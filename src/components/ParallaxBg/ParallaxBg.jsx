0c7609249c9499574643c9d7e6b5e8ca48feb485
JollyR0ger <kav993@gmail.com>
2023-04-19 17:28:36 +0300

    add bg wrapper

--- src/components/ParallaxBg/ParallaxBg.jsx
+++ src/components/ParallaxBg/ParallaxBg.jsx
@@ -3,26 +3,30 @@ import { useEffect, useRef } from 'react';
 import './styles.css';
 
 const ParallaxBg = () => {
-  const bg = useRef(null);
+  const bg0 = useRef(null);
+  const bg1 = useRef(null);
   const numDots = 100;
 
   const scatterDots = (amount) => {
-    console.log('bg', bg)
     for (let i = 0; i < amount; i++) {
       const dot = document.createElement("div");
       dot.className = "parallax-bg__dot";
-      dot.style.left = Math.random() * bg.current.offsetWidth + "px";
-      dot.style.top = Math.random() * bg.current.offsetHeight + "px";
-      bg.current.appendChild(dot);
+      dot.style.left = Math.random() * bg1.current.offsetWidth + "px";
+      dot.style.top = Math.random() * bg1.current.offsetHeight + "px";
+      bg1.current.appendChild(dot);
     }
   }
 
   useEffect(() => {
-    console.log('mounted', bg);
+    console.log('mounted', bg1);
     scatterDots(numDots);
   }, []);
 
-  return <div ref={bg} className='parallax-bg' />
+  return (
+    <div ref={bg0}>
+      <div ref={bg1} className='parallax-bg' />
+    </div>
+  )
 };
 
 export default ParallaxBg;
