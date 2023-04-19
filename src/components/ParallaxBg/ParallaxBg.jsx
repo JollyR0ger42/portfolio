f73da7048a43f7caed6d67d743329ee8a3950789
JollyR0ger <kav993@gmail.com>
2023-04-19 16:04:02 +0300

    add stars generation

--- src/components/ParallaxBg/ParallaxBg.jsx
+++ src/components/ParallaxBg/ParallaxBg.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import './styles.css'
+import { useEffect, useRef } from 'react';
+
+import './styles.css';
 
 const ParallaxBg = () => {
-  return <div className='parallax-bg' />
+  const bg = useRef(null);
+  const numDots = 100;
+
+  const scatterDots = (amount) => {
+    console.log('bg', bg)
+    for (let i = 0; i < amount; i++) {
+      const dot = document.createElement("div");
+      dot.className = "parallax-bg__dot";
+      dot.style.left = Math.random() * bg.current.offsetWidth + "px";
+      dot.style.top = Math.random() * bg.current.offsetHeight + "px";
+      bg.current.appendChild(dot);
+    }
+  }
+
+  useEffect(() => {
+    console.log('mounted', bg);
+    scatterDots(numDots);
+  }, []);
+
+  return <div ref={bg} className='parallax-bg' />
 };
 
 export default ParallaxBg;
