3e8f9b2e6559a479f63bd1e39cdb4c9ca6301e7c
JollyR0ger <kav993@gmail.com>
2023-05-09 21:00:56 +0300

    animation speed tweeking

--- src/components/ParallaxBg/ParallaxBg.jsx
+++ src/components/ParallaxBg/ParallaxBg.jsx
@@ -5,9 +5,9 @@ const ParallaxBg = () => {
 
   return (
     <div className="parallax-bg">
-      < ParallaxLayer starsAmount={500} starRadius={1} starsSpeed={2} starsAcceleration={1} />
-      < ParallaxLayer starsAmount={250} starRadius={2} starsSpeed={5} starsAcceleration={2} />
-      < ParallaxLayer starsAmount={125} starRadius={4} starsSpeed={10} starsAcceleration={4} />
+      < ParallaxLayer starsAmount={500} starRadius={1} starsSpeed={5} starsAcceleration={1} />
+      < ParallaxLayer starsAmount={250} starRadius={2} starsSpeed={10} starsAcceleration={2} />
+      < ParallaxLayer starsAmount={125} starRadius={4} starsSpeed={20} starsAcceleration={4} />
     </div>
   )
 }
