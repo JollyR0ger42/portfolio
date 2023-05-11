f03316555b7846f1bdc120925acdc25e1796942b
JollyR0ger <kav993@gmail.com>
2023-05-11 15:33:09 +0300

    minor change

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {target: 'es2019',},
   plugins: [react()],
-  build: {
-    target: 'es2019'
-  }
 })
