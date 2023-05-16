46723125ad03499463c748084c21aadd4060f2a3
JollyR0ger <kav993@gmail.com>
2023-05-16 23:30:35 +0300

    png to avif

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.png";
+import logo from "./logo.avif";
 import github from "./github.svg";
 import web from "./web.svg";
 import menu from "./menu.svg";
@@ -9,29 +9,29 @@ import linkedin from "./linkedin.svg";
 import telegram from "./telegram.svg";
 import downloadPdf from "./downloadPdf.svg";
 
-import css from "./tech/css.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import vue from "./tech/vue.png";
+import css from "./tech/css.avif";
+import figma from "./tech/figma.avif";
+import git from "./tech/git.avif";
+import html from "./tech/html.avif";
+import javascript from "./tech/javascript.avif";
+import mongodb from "./tech/mongodb.avif";
+import nodejs from "./tech/nodejs.avif";
+import reactjs from "./tech/reactjs.avif";
+import redux from "./tech/redux.avif";
+import tailwind from "./tech/tailwind.avif";
+import typescript from "./tech/typescript.avif";
+import vue from "./tech/vue.avif";
 
-import brighta from "./company/brighta.png";
-import softserve from './company/softserve.png';
-import codecare from './company/codecare.png';
-import diia from './company/diia.png';
-import hzf from './company/hzf.png';
+import brighta from "./company/brighta.avif";
+import softserve from './company/softserve.avif';
+import codecare from './company/codecare.avif';
+import diia from './company/diia.avif';
+import hzf from './company/hzf.avif';
 
-import gol from "./gol.png";
-import cryptocounter from "./cryptocounter.png";
-import landing from "./landing.png";
-import asteroids from './asteroids.png';
+import gol from "./gol.avif";
+import cryptocounter from "./cryptocounter.avif";
+import landing from "./landing.avif";
+import asteroids from './asteroids.avif';
 
 export {
   logo,
