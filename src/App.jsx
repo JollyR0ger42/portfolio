70f479d64e12a843b5efb57b35ace7b890412f4c
JollyR0ger <kav993@gmail.com>
2023-05-17 19:47:24 +0300

    async css to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
 
 import { Experience, About, Navbar, Works, Footer, ParallaxBg } from "./components";
-import "./index.css";
 
 const App = () => {
+  useEffect(() => {
+    let resizeEventId;
+    const loadStyles = async () => {
+      await import("./index.css");
+      resizeEventId = setTimeout(() => window.dispatchEvent(new Event('resize')), 100)
+    };
+    loadStyles();
+    return () => clearTimeout(resizeEventId);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='relative z-0'>
